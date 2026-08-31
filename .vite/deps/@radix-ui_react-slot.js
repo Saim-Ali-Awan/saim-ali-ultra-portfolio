ac7c733c33751c495e3b4900cc62919e59efb5e8
Saim Ali <saimalimalikawan786@gmail.com>
2026-08-31 23:01:39 +0500

    chore: remove vite cache from git tracking and update gitignore

---
+++ .vite/deps/@radix-ui_react-slot.js
@@ -0,0 +1,15 @@
+import {
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+} from "./chunk-23CTII2U.js";
+import "./chunk-ZTOBGZBR.js";
+import "./chunk-ZPWHCABL.js";
+export {
+  Slot as Root,
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+};
